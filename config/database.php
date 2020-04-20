620d23a9e5ad51f31951c7e93789409af89afcbf
deva7mad <dev.elknany@gmail.com>
2020-04-21 00:48:34 +0200

    add ui & config tenancy package

--- config/database.php
+++ config/database.php
@@ -43,6 +43,21 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'system' => [
+            'driver' => 'mysql',
+            'host' => env('TENANCY_HOST', '127.0.0.1'),
+            'port' => env('TENANCY_PORT', '3306'),
+            'database' => env('TENANCY_DATABASE', 'tenancy'),
+            'username' => env('TENANCY_USERNAME', 'tenancy'),
+            'password' => env('TENANCY_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
